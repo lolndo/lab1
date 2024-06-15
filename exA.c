c3f509c1e6fcdb585413b9b700efb94dbcfbc526
lolndo <aleksandrmilk007@gmail.com>
2024-06-15 15:50:38 +0300

    updaateB code file

--- exA.c
+++ exA.c
@@ -1,42 +1,37 @@
-#include <math.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 #define A -5
 #define B 5
 #define H 0.1
 
-double F(double x) 
+double F(double x)
 {
-  return x * exp(x) + 2 * sin(x) - sqrt(sqrt(pow(x * x * x - x * x, 2)));
+    return x*exp(x)+2*sin(x)-sqrt(sqrt(pow(x*x*x-x*x,2)));
 }
-int main() 
+int main()
 {
-  int count;
-  double x, y, m, n, M, N;
-  count = 0;
-  printf("введите число M: ");
-  scanf("%lf", &m);
-  printf("введите число N: ");
-  scanf("%lf", &n);
-  if (m < n) 
-  {
-    M = m;
-    N = n;
-  } 
-  else 
-  {
-    N = m;
-    M = n;
-  }
+  double x,y,s,sum=0;
   printf("значение аргумента     значение функции\n");
-  for (x = A; x <= B; x += H) 
+  for (x=A; x<=B; x+=H)
   {
-    y = F(x);
-    printf("%10.4f            %10.4f\n", x, y);
-    if (x >= M && x <= N) 
+    y=F(x);
+    char a[12],str1,str2,str3,str4;
+    int val1,val2,val3,val4;
+    printf("%10.4f            %10.4f\n",x,y);
+    sprintf(a,"%10.4f",y);//расскладываем число как элементы массива
+    str1 = a[6];//выписываем цифры числа после запятой 
+    str2 = a[7];
+    str3 = a[8];
+    str4= a[9];
+    val1=atoi(&str1);//преобразуем символ в число
+    val2=atoi(&str2);
+    val3=atoi(&str3);
+    val4=atoi(&str4);
+    if (y>0 && (val1>5 || ((val1==5 && (val2>=1 && val2<=9))||(val1==5 && (val3>=1 && val3<=9))||(val1==5 && (val4>=1 && val4<=9)))))
     {
-      count += 1;
+      sum=sum + y;
     }
   }
-  printf(
-      "количество значений функции которые находятся на отрезке от M до N %d", count);
+  printf("Сумма значений функции, которые имеют дробную часть, большую 0,5 равна %10.4f",sum);
 }
