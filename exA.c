89571dae830258fb2d33c5134629096127c3e6c3
lolndo <aleksandrmilk007@gmail.com>
2024-06-15 15:46:53 +0300

    UPDATE code file

--- exA.c
+++ exA.c
@@ -4,10 +4,12 @@
 #define B 5
 #define H 0.1
 
-double F(double x) {
+double F(double x) 
+{
   return x * exp(x) + 2 * sin(x) - sqrt(sqrt(pow(x * x * x - x * x, 2)));
 }
-int main() {
+int main() 
+{
   int count;
   double x, y, m, n, M, N;
   count = 0;
@@ -15,22 +17,26 @@ int main() {
   scanf("%lf", &m);
   printf("введите число N: ");
   scanf("%lf", &n);
-  if (m < n) {
+  if (m < n) 
+  {
     M = m;
     N = n;
-  } else {
+  } 
+  else 
+  {
     N = m;
     M = n;
   }
   printf("значение аргумента     значение функции\n");
-  for (x = A; x <= B; x += H) {
+  for (x = A; x <= B; x += H) 
+  {
     y = F(x);
     printf("%10.4f            %10.4f\n", x, y);
-    if (x >= M && x <= N) {
+    if (x >= M && x <= N) 
+    {
       count += 1;
     }
   }
   printf(
-      "количество значений функции которые находятся на отрезке от M до N %d",
-      count);
+      "количество значений функции которые находятся на отрезке от M до N %d", count);
 }
